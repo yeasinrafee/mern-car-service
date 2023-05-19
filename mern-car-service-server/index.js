97b48b143498f5ae2f526bc4ffd66d48ee348dfc
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-19 17:03:06 +0600

    Reciving data and storing to database

--- mern-car-service-server/index.js
+++ mern-car-service-server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -30,10 +30,29 @@ async function run() {
     //---------------------------------------------------------------------
 
     const serviceCollection = client.db("carService").collection("Services");
+    const bookingCollection = client.db("carService").collection("bookings");
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      //   const option = {
+      //     projection: { _id: 0, title: 1, img: 1, price: 1 },
+      //   };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Bookings
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
 
