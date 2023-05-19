ae4bfb05adbaaa088554ac344375fc2e8786aea1
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-19 09:57:37 +0600

    Dynamic url for data

--- mern-car-service-server/index.js
+++ mern-car-service-server/index.js
@@ -27,6 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //---------------------------------------------------------------------
+
+    const serviceCollection = client.db("carService").collection("Services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //---------------------------------------------------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +45,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
